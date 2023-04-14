e7a5732e9ef09c47715e098a767822c1a2eea4cb
lnntrinh0112 <avs73ltqb@gmail.com>
2023-04-14 21:24:47 +0700

    TD _ Clear Form and clear Delete Button

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -45,6 +45,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleleIngredient(this.editItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
