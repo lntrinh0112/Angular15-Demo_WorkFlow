6d1eead90baf697f1287773cda0e9c1e0e966c18
lnntrinh0112 <avs73ltqb@gmail.com>
2023-04-21 10:56:08 +0700

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -26,22 +27,43 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknown error occurred!';
-          if (!errorRes || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already!';
-              break;
-            default:
-              errorMessage = 'Unknown error occurred!';
-              break;
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
+    // .pipe(catchError((errorRes) => this.handleError(errorRes)))
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDfZVfvoDGH_LQ9NrwkSyIgOXBMCBbi0NI',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already!';
+        break;
+
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist!';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct!';
+        break;
+      default:
+        errorMessage = 'Unknown error occurred!';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
